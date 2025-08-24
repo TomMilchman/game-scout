a3fe6b2baf357d1b2b37ac8d3daa7d0809b11eb4
Tom Milchman <tom.milchman@gmail.com>
2025-08-24 20:02:49 +0300

    Use provided last_updated value instead of NOW() in price upserts

--- src/db/prices.ts
+++ src/db/prices.ts
@@ -9,7 +9,7 @@ export async function upsertGamePrices(
             (gamePrice) =>
                 sql`
                     INSERT INTO prices (game_id, store, base_price, current_price, currency, url, last_updated)
-                    VALUES (${gamePrice.game_id}, ${gamePrice.store}, ${gamePrice.base_price}, ${gamePrice.current_price}, ${gamePrice.currency}, ${gamePrice.url}, NOW())
+                    VALUES (${gamePrice.game_id}, ${gamePrice.store}, ${gamePrice.base_price}, ${gamePrice.current_price}, ${gamePrice.currency}, ${gamePrice.url}, ${gamePrice.last_updated})
                     ON CONFLICT (game_id, store) DO
                     UPDATE SET
                         base_price = EXCLUDED.base_price,

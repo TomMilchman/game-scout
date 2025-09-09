eff78d483394247a50645a64263914d70397591c
Tom Milchman <tom.milchman@gmail.com>
2025-09-09 17:05:02 +0300

    Extract scraping service to another project (outside of this repo)

--- src/services/scraper-service/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export function generateGameSlug(title: string, spaceReplacer: string) {
-    return title
-        .toLowerCase()
-        .replace(/'/g, "") // remove apostrophes
-        .replace(/[^a-z0-9 ]/g, "") // remove other non-alphanumeric characters except space
-        .trim()
-        .replace(/\s+/g, spaceReplacer); // replace spaces with underscores
-}

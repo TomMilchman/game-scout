bdd602bf9e6bab786a03147db465f3963366800c
Tom Milchman <tom.milchman@gmail.com>
2025-09-14 13:01:25 +0300

    Add dynamic rendering to game page

--- src/app/(protected)/game/[gameId]/page.tsx
+++ src/app/(protected)/game/[gameId]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { fetchGamesByIdsOrScrape } from "@/app/server/games";
 import GamePricesServer from "@/components/gamePricesServer";
 import { auth } from "@clerk/nextjs/server";
